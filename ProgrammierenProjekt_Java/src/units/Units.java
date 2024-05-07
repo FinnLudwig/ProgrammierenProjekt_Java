925fdf481c45737f1ebc08e29bc07705c826672d
Finn Ludwig <167445067+FinnLudwig@users.noreply.github.com>
2024-05-07 14:40:07 +0200

    Added map, terrain, player, game
    map: 3 Maps, Map Superclass
    terrain: 8 Terrains, Terrain Superclass
    player: Player Class -Player 1/Player 2 Objects will be created in Game
    game: Game Class -Game Object will be created in Main, Game will implement Rules, Player status etc.

--- ProgrammierenProjekt_Java/src/units/Units.java
+++ ProgrammierenProjekt_Java/src/units/Units.java
@@ -1,6 +1,40 @@
 package units;
 
 public class Units {
+	
+//Attributes 
+private int price;
+private int health;
+private int movementRange;
+private int attackRange;
 
+
+
+
+/*Getter and Setter for Attributes (price, health, movementRange, attackRange)*/ 
+public int getPrice() {
+	return price;
+}
+public void setPrice(int price) {
+	this.price = price;
+}
+public int getHealth() {
+	return health;
+}
+public void setHealth(int health) {
+	this.health = health;
+}
+public int getMovementRange() {
+	return movementRange;
+}
+public void setMovementRange(int movementRange) {
+	this.movementRange = movementRange;
+}
+public int getAttackRange() {
+	return attackRange;
+}
+public void setAttackRange(int attackRange) {
+	this.attackRange = attackRange;
+}
 }
  
