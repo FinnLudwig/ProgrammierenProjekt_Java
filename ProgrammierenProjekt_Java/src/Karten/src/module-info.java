77c9d35fcac0c93b0793464935fbcac575aaef8a
Katya Karavaeva <123589038+kkaravaeva@users.noreply.github.com>
2024-06-15 19:08:06 +0200

    Delete ProgrammierenProjekt_Java/src/Karten/src directory

--- ProgrammierenProjekt_Java/src/Karten/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Projekt_3 {
-	requires transitive javafx.graphics;
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	
-	exports application;
-}
